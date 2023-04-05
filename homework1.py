471defd89adfaf6980e2e93144a522247c51d438
wylasthegreat <129912986+wylasthegreat@users.noreply.github.com>
2023-04-04 17:41:42 -0700

    Homework 1
    I didn't do 2.2, I was confused about how to create variables strictly for user input, and user input itself.

---
+++ homework1.py
@@ -0,0 +1,14 @@
+a = " ||"
+b = "-----"
+print a
+print b
+print a
+print b
+print a
+
+firstname = 'Wylas'
+lastname = 'Sei'
+month = 'January'
+day = '10'
+year = '2010'
+print firstname, lastname+' was born on', month, day, year
