89160057d3beae998dfe4dadd858713478e0285a
Hoogus <129913089+Hoogus@users.noreply.github.com>
2023-04-04 17:40:59 -0700

    Add files via upload

---
+++ Hello world.py..py
@@ -0,0 +1,72 @@
+#Hello world
+##print('hello world')
+
+#TicTacToeBoard
+##print('''
+##    ||
+##   ----
+##    ||
+##   ----
+##    || ''')
+
+#Variables, its not shorter but idk what to do to make it shorter
+##a = '''
+##    ||
+##   ----
+##    ||      
+##   ----
+##    || '''
+##
+##print(a)
+
+#Introductions!!! Not sure if i understood the assignment
+##Fname = input('Enter your first name:')
+##Lname = input('Enter your last name:')
+##print('Please enter your date of birth:')
+##Mo = input('Month?')
+##Day = int(input('Day?'))
+##Year = int(input('Year?'))
+##
+##print(Fname, Lname,'was born on', Mo, Day, Year)
+
+##ROCK PAPER SCISSORS
+##Player1 = input('Player1, please enter either rock, paper, or scissors:').lower()
+##Player2 = input('Player2, please enter either rock, paper, or scissors:').lower()
+##
+##if Player1 == 'rock' and Player2 == 'scissors':
+##    print('Player1 wins')
+##
+##elif Player1 == 'scissors' and Player2 == 'paper':
+##    print('Player1 wins')
+##
+##elif Player1 == 'paper' and Player2 == 'rock':
+##    print('Player1 wins')
+##
+##elif Player2 == 'rock' and Player1 == 'scissors':
+##    print('Player2 wins')
+##
+##elif Player2 == 'scissors' and Player1 == 'paper':
+##    print('Player2 wins')
+##
+##elif Player2 == 'paper' and Player1 == 'rock':
+##    print('Player2 wins')
+##
+##elif Player1 == 'paper' and Player2 == 'paper':
+##    print('Tie')
+##
+##elif Player1 == 'rock' and Player2 == 'rock':
+##    print('Tie')
+##
+##elif Player1 == 'rock' and Player2 == 'rock':
+##    print('Tie')
+##
+##else:
+##    print('This is not a valid object selection')
+    
+
+
+
+
+
+
+
