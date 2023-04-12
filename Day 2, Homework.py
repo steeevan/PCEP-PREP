de6e44f82a6f63e7000a702d9f3d03e5d30d6025
mattSCoder <129913100+mattSCoder@users.noreply.github.com>
2023-04-11 17:18:56 -0700

    Add files via upload

---
+++ Day 2, Homework.py
@@ -0,0 +1,134 @@
+# Homework 2
+# Matt Shen
+
+
+# 3. Calculate A of circle from radius
+##r = int(input("Enter a radius for a circle: "))
+##circleA = 3.14*r**2
+##print(circleA)
+
+
+# 4. Reverse name
+##firstName = input("Enter first name: ")
+##lastName = input("Enter last name: ")
+##fullName = firstName + " " + lastName
+##reverseName = fullName[::-1]
+##print(reverseName)
+
+
+# 6. First and last color
+##color_list = ["Red", "Green", "Black", "White"]
+##print(color_list[0] + ", " + color_list[-1])
+
+
+# 7. Integer n
+##n = int(input("Enter a random number: "))
+##sum = (n) + (n*10 + n) + (n*100 + n*10 + n)
+##print(sum)
+
+
+# 8. Volume of sphere
+# V = 4/3 * π * r^3
+##r = 6
+##sphereV = 4/3 * 3.14 * 6**3
+##print(sphereV)
+
+
+# 9. calculate 17 difference
+# How to do absolute value
+##number = int(input("Enter a number: "))
+##difference = number - 17
+##if number > 17:
+##    difference *= 2
+##    print(difference)
+##else:
+##    print(difference)
+
+
+# 10. Calculate sum of 3
+##one = int(input("Enter a number: "))
+##two = int(input("Enter a number: "))
+##three = int(input("Enter a number: "))
+##sum = one + two + three
+##if one == two == three:
+##    sum *= 3
+##    print(sum)
+##else:
+##    print(sum)
+
+
+# 11. Odd or even
+##num = int(input("Enter a number: "))
+##oddOrEven = num % 2
+##if oddOrEven == 1:
+##    print("Odd")
+##elif oddOrEven == 0:
+##    print("Even")
+
+
+# 13.
+##hi = [1, 2, 3, 5, 6]
+##if 4 in hi:
+##    print("True")
+##else:
+##    print("False")
+
+
+# 15. List printing
+##numbers = [1, 4, 820, 3, 63, 0913, 9372, 293, 128, 134,
+##           277, 835, 211, 11, 237, 902, 153, 234]
+##
+##n = ''
+##for n 
+
+
+# 16. triangle A
+##base = int(input("Enter a base: "))
+##height = int(input("Enter a height: "))
+##area = base * height / 2
+##print(area)
+
+
+# 18. LCM
+##num1 = int(input("Enter a number: "))
+##num2 = int(input("Enter a number: "))
+##while num1 != num2:
+    
+
+# 19. sum 0
+##num1 = int(input("Enter a number: "))
+##num2 = int(input("Enter a number: "))
+##num3 = int(input("Enter a number: "))
+##
+##total = num1 + num2 + num3
+##
+##if num1 == num2 and num3 != num1 or num2 == num3 and num1 != num2 \
+##   or num3 == num1 and num2 != num1:
+##    total = 0
+##    print(total)
+##else:
+##    print(total)
+
+
+# 20. sum 20
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+total = num1 + num2
+
+if total > 15 and total < 20:
+    print(20)
+else: print(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
