7badb2e0b10d7c264d3e9218e5607c4165a6a1ab
Hoogus <129913089+Hoogus@users.noreply.github.com>
2023-04-11 18:00:44 -0700

    Homework number 2
    My questions are in the file

---
+++ Homework python 2.py
@@ -0,0 +1,87 @@
+#I don't know how to do 1, 2, 3, 4
+#----------------------------------------------------
+
+##First = input('Please enter your first name').lower()
+##Last = input('Please enter your last name').lower()
+##print(Last[:100], First[:100])
+#----------------------------------------------------
+#5 I don't know how to do it
+#----------------------------------------------------
+#6
+##print('Red and Black')
+#------------------------
+#7
+#I don't know why but i got 610
+##n = int(input('Please enter any number'))
+##
+##value = n+n*11+n*110
+##print(value)
+#------------------------
+#8 I have no idea how to do this
+#9 Im not sure what you mean by the last sentece
+#----------------------------------------------------
+#10
+##fn = int(input('Please enter a number'))
+##sn = int(input('Please enter a second number'))
+##tn = int(input('Please enter a third number'))
+##
+##value = fn+sn+tn
+##v = value*3
+##
+##if fn == tn and sn:
+##         print(v)
+##elif fn != tn and sn:
+##    print(value)
+#----------------------------------------------------
+#11
+#I don't know how to do it
+#12
+#What do you mean by "passed letter")
+#13 I don't know how to do it
+#14 I forgot what concatenating meant
+#15 I have no Idea how to even start
+#16
+##base = int(input('Please enter a measurement for the base of a traingle'))
+##height = int(input('Please enter a measurement for the hight of a traingle'))
+##
+##step = base*height
+##two = step/2
+#I don't know how to fix it
+#17 Do i just do greater than or less than? Is there a way to calculate or find the GCD?
+#18 How do i calculate that on python
+#19
+##fn = int(input('Please enter a number'))
+##sn = int(input('Please enter a second number'))
+##tn = int(input('Please enter a third number'))
+##
+##value = fn+sn+tn
+##
+##if fn == sn or tn:
+##    value = 0
+##elif sn == fn or tn:
+##    value = 0
+##elif tn == sn or fn:
+##    value = 0
+##
+##
+##print(value)
+#20
+##fn = int(input('Please enter a number'))
+##sn = int(input('Please enter a second number'))
+##value = fn+sn
+##
+##if value == 15 or 16 or 17 or 18 or 19 or 20:
+##    value = 20
+##
+##print(value)
+
+
+
+
+
+         
+
+
+
+
+
