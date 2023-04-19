b6336f4885e621431bba5ccd074bd4dfc95f3068
Hoogus <129913089+Hoogus@users.noreply.github.com>
2023-04-18 17:17:20 -0700

    Add files via upload
    Basically, I struggled with everything.

---
+++ Homework3.py
@@ -0,0 +1,40 @@
+#1
+##sample = input('Please enter a phrase')
+###I don't know how to check for 'ing'
+##if sample 
+##
+##if 3 <= len(sample):
+##    print(sample,'ing')
+#----------------------------------------
+#2
+#I don't know what a substring is
+#-----------------------------------------
+#3
+#I don't know how to check for things in list
+#-------------------------------------------
+#4
+#Whats the n'th index?? Whats a nonempty string?
+#-------------------------------------------
+#5
+#I don't know what you mean by "odd index values"
+#----------------------------------------------
+#6-10
+#I genuinely don't know how to do it. Im not very good with dictionarys
+#-----------------------------------------------
+#11
+#I remember we did this in class but I forgot how to do it
+#-------------------------------------------------
+#12
+##lol = ("red", "brown", "blue")
+##string = lol
+##print(string)
+#I was not very sure what to do, and what you meant by converting it to a string.
+#---------------------------------------------------
+#13
+#the wording confused me. How do you get the 4th element from the last element?
+#----------------------------------------------
+#14
+#Whats a colon
+#-------------------------------------------------------
+#15
+#I don't know how to check in lists, tuples, and strings
