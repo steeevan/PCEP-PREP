32a34ff5efc4b74b6d76ee66c396c35be32a68aa
Hoogus <129913089+Hoogus@users.noreply.github.com>
2023-05-23 17:18:47 -0700

    Add files via upload

---
+++ Homework somin somin.py
@@ -0,0 +1,72 @@
+# HomeWork 4
+#1
+##import random
+##money = int(input('Please enter a number for the min'))
+##cash = int(input('Please enter a number for the max'))
+##
+##def UserInput(mom = 10, max = 20):
+##
+##    if money and cash != pass
+##      print(money)
+##
+#I didn't really know what to do for this one
+#---------------------------------------------
+#2
+##import random
+##
+##def generate_number(min = 30, max = 40):
+##    print(random.randint(min,max))
+#It kept saying RESTART: C:/Users/maxsh/AppData/Local/Programs/Python/Python310/Homework somin somin.py
+#---------------------------------------------
+#3
+##while pass:
+##money = int(input('Please enter a number for the min'))
+##cash = int(input('Please enter a number for the max'))
+##if money and cash == 0:
+##    break
+##def generate_number(min = 30, max = 40):
+##    pass
+##def UserInput(min = 10, max = 20):
+##    pass
+#-------------------------------------------------
+#4
+##Brah():
+##    age = int(input('What is your age?: '))
+##    name = input('What is your name?')
+##
+##lol(min = 0, max = 100):
+##    hundred = age * 1000
+##    print('
+#Idk how to continue or how to do it
+#-----------------------------------------------
+#5
+##def number():
+##    please = int(input('Please enter a number'))
+##    
+##    return please
+
+#Idk how to do the second part
+#6
+#I have no idea
+#---------------------------------------------------
+#Homework 5
+#1
+#I don't know how to do it
+#---------------------------------
+#2
+#I don't even know where to start
+#--------------------------------
+#3
+#I don't know
+#---------------
+#4
+#I don't know how to use booleans to identify if the user input is less than 1.
+
+    
+
+    
+    
+    
+
+
+    
