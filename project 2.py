420f5bac67f19584b49ec8ca2a5180a14d7fc169
Hoogus <129913089+Hoogus@users.noreply.github.com>
2023-07-01 00:03:37 -0700

    Add files via upload

---
+++ project 2.py
@@ -0,0 +1,29 @@
+
+def intro():
+    print('''Project 2 Multi-Functional Calculator: Arithmetic Operations
+and Area Calculation. The Multi-Functional Calculaotr is a powerful tool
+designed to assist users in performing arithmetic calculations and finding
+the areas of different geomitric shapes. It combines the convience of a standard
+calculator with the added functionality of shape-specific area calculations''')
+
+#I don't know how to continue
+
+def add():
+
+def subtract():
+
+def multiple():
+
+def divide():
+
+def rectArea():
+
+def sqrArea():
+
+def circArea():
+
+def triArea():
+
+def cubeVol():
+
+def rectVol():
